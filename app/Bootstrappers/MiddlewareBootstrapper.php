4b84097120d5f2538cee2243290f7b053387e685
FlorianPalabost <palabostf@gmail.com>
2025-10-10 12:55:33 +0200

    chore: setup cors

--- app/Bootstrappers/MiddlewareBootstrapper.php
+++ app/Bootstrappers/MiddlewareBootstrapper.php
@@ -6,11 +6,13 @@ namespace App\Bootstrappers;
 
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Middleware\HandleCors;
 
 class MiddlewareBootstrapper
 {
     public function __invoke(Middleware $middleware): void
     {
+        $middleware->api(prepend: HandleCors::class);
         // $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         // $middleware->web(append: [
