26db143caa0c394674ac12d900760606fc9a2f89
FlorianPalabost <palabostf@gmail.com>
2025-10-13 21:31:49 +0200

    chore: enable stateful api middlewares

--- app/Bootstrappers/MiddlewareBootstrapper.php
+++ app/Bootstrappers/MiddlewareBootstrapper.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Bootstrappers;
 
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Middleware\HandleCors;
 
 class MiddlewareBootstrapper
@@ -13,10 +12,7 @@ class MiddlewareBootstrapper
     public function __invoke(Middleware $middleware): void
     {
         $middleware->api(prepend: HandleCors::class);
+        $middleware->statefulApi();
         // $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
-
-        // $middleware->web(append: [
-        //     AddLinkHeadersForPreloadedAssets::class,
-        // ]);
     }
 }
