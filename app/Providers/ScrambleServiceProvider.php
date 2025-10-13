658cde8dca95cc5f669b2b63c962f9cb4f5708fa
FlorianPalabost <palabostf@gmail.com>
2025-10-13 21:31:21 +0200

    refactor: enable auth with bearer token in scramble

---
+++ app/Providers/ScrambleServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Support\ServiceProvider;
+
+class ScrambleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void {}
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Scramble::registerApi('v1', [
+            'api_path' => 'api/v1',
+        ]);
+
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $openApi) {
+                $openApi->secure(
+                    SecurityScheme::http('bearer')
+                );
+            });
+    }
+}
