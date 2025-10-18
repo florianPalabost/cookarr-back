7abc1b7e01544a8b5999ae1a7b91fff83b881916
FlorianPalabost <palabostf@gmail.com>
2025-10-18 12:46:46 +0200

    test: setup auth refresh & register controller tests

---
+++ tests/Feature/Http/Controllers/Api/V1/Auth/RefreshAuthTokenControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+describe('Http > Controllers > V1 > Auth > RefreshAuthTokenController', function () {
+    it('should refresh auth tokens with a valid refresh token', function () {});
+
+    it('should not refresh auth tokens with an invalid refresh token', function () {});
+})->group('controllers', 'v1', 'auth');
--- tests/Feature/Http/Controllers/Api/V1/Auth/RegisterControllerTest.php
+++ tests/Feature/Http/Controllers/Api/V1/Auth/RegisterControllerTest.php
@@ -2,8 +2,29 @@
 
 declare(strict_types=1);
 
-test('example', function () {
-    $response = $this->get('/');
+use Symfony\Component\HttpFoundation\Response;
 
-    $response->assertStatus(200);
-});
+describe('Http > Controllers > V1 > Auth > RegisterController', function () {
+    it('should register a new user', function () {
+        // Arrange
+        $payload = [
+            'name'                  => 'John Doe',
+            'email'                 => 'jdoe@me.com',
+            'password'              => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        // Act
+        /** @var Tests\TestCase $this */
+        $response = $this->postJson(uri: '/api/v1/auth/register', data: $payload);
+
+        // Assert
+        expect($response->getStatusCode())->toBe(Response::HTTP_CREATED);
+        // TODO: custom expect for success api response
+    });
+
+    it('should not pass validation if password mismatch', function () {});
+
+    it('should not pass validation if email already exists', function () {});
+
+})->group('controllers', 'v1', 'auth');
