e33f7bed573a09f42909335146a82aa674c479bc
FlorianPalabost <palabostf@gmail.com>
2025-10-18 12:45:51 +0200

    test: add models relations tests

--- tests/Feature/Models/IngredientTest.php
+++ tests/Feature/Models/IngredientTest.php
@@ -2,4 +2,21 @@
 
 declare(strict_types=1);
 
-describe('Models > Ingredient', function () {})->group('models');
+use App\Models\Ingredient;
+use App\Models\Recipe;
+
+describe('Models > Ingredient', function () {
+    it('has recipes', function () {
+        // Arrange
+        $recipesCountCreated = 3;
+        $ingredient = Ingredient::factory()->create();
+        $ingredient->recipes()->saveMany(Recipe::factory($recipesCountCreated)->make());
+
+        // Act
+        $recipes = $ingredient->recipes;
+
+        // Assert
+        expect($recipes)->toHaveCount($recipesCountCreated)
+            ->and($recipes->first())->toBeInstanceOf(Recipe::class);
+    });
+})->group('models');
--- tests/Feature/Models/RecipeTest.php
+++ tests/Feature/Models/RecipeTest.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Models\Ingredient;
 use App\Models\Recipe;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 
 describe('Models > Recipe', function () {
     it('has a user', function () {
@@ -15,5 +17,19 @@ describe('Models > Recipe', function () {
 
         // Assert
         expect($user)->toBeInstanceOf(User::class);
+    });
+
+    it('has ingredients', function () {
+        // Arrange
+        $ingredientsCountCreated = 3;
+        $recipe = Recipe::factory()->create();
+        $recipe->ingredients()->saveMany(Ingredient::factory($ingredientsCountCreated)->make());
+
+        // Act
+        $ingredients = $recipe->ingredients;
+
+        // Assert
+        expect($ingredients)->toBeInstanceOf(Collection::class)
+            ->and($ingredients)->toHaveCount($ingredientsCountCreated);
     });
 })->group('models');
